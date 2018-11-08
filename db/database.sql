5b7cff6e6243cd8316aa8d7836c1a133cd48d965
“Hugo <hugohoffmann04@gmail.com>
2018-11-08 20:40:34 -0200

    Alterado arquivo database.sql

--- db/database.sql
+++ db/database.sql
@@ -14,21 +14,21 @@ CONSTRAINT fk_pessoa FOREIGN KEY (pescodigo) REFERENCES tbpessoa (pescodigo)
 CREATE TABLE IF NOT EXISTS tbcarro (
 carcodigo integer NOT NULL, 
 cardescricao varchar(50),
-carvalor numeric(17,2), //valor diária, lembrar de específicar isso na tabela de amostra
-CONSTRAINT pk_tbproduto PRIMARY KEY (procodigo)
+carvalor numeric(17,2), 
+CONSTRAINT pk_tbcarro PRIMARY KEY (carcodigo)
 );
 CREATE TABLE IF NOT EXISTS tbpedido (
 pedcodigo integer NOT NULL, 
 clicodigo integer NOT NULL,
 peddata date NOT NULL,
 CONSTRAINT pk_tbpedido PRIMARY KEY (pedcodigo),
-CONSTRAINT fk_tbcliente FOREIGN KEY (clicodigo) REFERENCES tbcliente (clicodigo),
+CONSTRAINT fk_tbcliente FOREIGN KEY (clicodigo) REFERENCES tbcliente (clicodigo)
 );
 CREATE TABLE IF NOT EXISTS tbpedidocarro (
 pedcodigo integer NOT NULL,
 carcodigo integer NOT NULL,
 pepquantidade smallint,
-CONSTRAINT pk_pedido_produto PRIMARY KEY (pedcodigo, procodigo),
+CONSTRAINT pk_pedido_carro PRIMARY KEY (pedcodigo, carcodigo),
 CONSTRAINT fk_tbpedido FOREIGN KEY (pedcodigo) REFERENCES tbpedido (pedcodigo) ON DELETE CASCADE,
-CONSTRAINT fk_tbproduto FOREIGN KEY (carcodigo) REFERENCES tbproduto (carcodigo)
+CONSTRAINT fk_tbcarro FOREIGN KEY (carcodigo) REFERENCES tbcarro (carcodigo)
 );
