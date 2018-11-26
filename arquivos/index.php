0c92bdb43359aa171c3a8da5e71770d6d60a0073
“Hugo <hugohoffmann04@gmail.com>
2018-11-25 22:29:34 -0200

    Update index.php

--- arquivos/index.php
+++ arquivos/index.php
@@ -12,7 +12,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href='../estilo/estiloHome.css'>
-        <script type="text/javascript" src="../js/validaCampo.js"></script>
+        <script type="text/javascript" src="../js/clientes.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </head>
     <header>
